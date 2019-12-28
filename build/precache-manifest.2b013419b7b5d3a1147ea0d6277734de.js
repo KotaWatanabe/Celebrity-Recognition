8d5bf3d61e0ef71033b3ef79547440b6a8b2fddc
Kota Watanabe <kota3316@gmail.com>
2019-12-27 17:54:37 -0800

    build

---
+++ build/precache-manifest.2b013419b7b5d3a1147ea0d6277734de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe54d5c9c8e2d80509ea6bf5631375e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f214a65bb910910bdd",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6a695176b30723ce72c1",
+    "url": "/static/css/main.75051e9d.chunk.css"
+  },
+  {
+    "revision": "22f214a65bb910910bdd",
+    "url": "/static/js/2.728bb53e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.728bb53e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a695176b30723ce72c1",
+    "url": "/static/js/main.98c945fd.chunk.js"
+  },
+  {
+    "revision": "8ca41927d6d64f70e65f",
+    "url": "/static/js/runtime-main.48093da9.js"
+  },
+  {
+    "revision": "b485b2dbdbbaf0d982c6f6930d5e1a47",
+    "url": "/static/media/logo.b485b2db.png"
+  }
+]);
